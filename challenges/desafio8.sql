00761a0141b55c21bb0915bfcdb0bc77b9465418
Wlauber <wlaubersamuel@gmail.com>
2023-05-18 16:46:09 -0300

    requisito 08 concluído

---
+++ challenges/desafio8.sql
@@ -0,0 +1,9 @@
+SELECT artistas.nome_artista AS artista,
+albums.nome_album AS album
+
+FROM SpotifyClone.artistas
+
+JOIN SpotifyClone.albums ON albums.artista_id = artistas.artista_id
+
+WHERE artistas.nome_artista = 'Elis Regina';
+ 
